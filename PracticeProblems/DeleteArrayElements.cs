57fa27c4a8b0bf7106476bbcf3cedb0793e5bb4e
Shivani21-hude <shivani21hude@gmail.com>
2023-03-24 09:32:25 +0530

    [Shivani]-Delete corresponding elements from the array using int,double,char

---
+++ PracticeProblems/DeleteArrayElements.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblems
+{
+    internal class DeleteArrayElements
+    {
+        public static void DeleteIntElements(int[] arr , int delete)
+        { 
+
+            int k = 0;
+            int[] result=new int[arr.Length-1];
+
+            Console.WriteLine("The remaining elements are : ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] != delete)
+                {
+                    result[k] = arr[i];
+                    k++;
+                }
+            }
+           
+            foreach(int i in result)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
---
+++ PracticeProblems/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblems
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                
+               
+                Console.WriteLine("\n \nChoose a number from following \n 1) To print remaining int elements after deleteing 1 element \n");
+
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("How many elements to add into array");
+                        int size = Convert.ToInt32(Console.ReadLine());
+                        int[] arr = new int[size];
+
+                        Console.WriteLine("Enter the elements 1 by 1 :");
+                        for (int i = 0; i < size; i++)
+                        {
+                            arr[i] = Convert.ToInt32(Console.ReadLine());
+                        }
+
+                        Console.WriteLine("\n please pickup any one element from this array");
+                        int delete = Convert.ToInt32(Console.ReadLine());
+                        DeleteArrayElements.DeleteIntElements(arr,delete);
+                        break;
+                }
+            }
+        }
+    }
+}
