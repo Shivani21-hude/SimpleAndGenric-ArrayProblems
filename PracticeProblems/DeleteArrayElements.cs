906aa5912c3c3c197434649211f00e53c161f47b
Shivani21-hude <shivani21hude@gmail.com>
2023-03-24 09:34:47 +0530

    [Shivani]-Delete corresponding elements from the array using int,double,char

--- PracticeProblems/DeleteArrayElements.cs
+++ PracticeProblems/DeleteArrayElements.cs
@@ -8,12 +8,12 @@ namespace PracticeProblems
 {
     internal class DeleteArrayElements
     {
-        public static void DeleteIntElements(int[] arr , int delete)
-        { 
-
-            int k = 0;
-            int[] result=new int[arr.Length-1];
-
+        public static void DeleteIntElements(int[] arr , int delete) 
+        {
+                                                                    //size = 3
+            int k = 0;                                              //arr =  1 4 5
+            int[] result=new int[arr.Length-1];                     //delete = 4
+                                                                    //result = 1 5
             Console.WriteLine("The remaining elements are : ");
             for (int i = 0; i < arr.Length; i++)
             {
@@ -26,7 +26,51 @@ namespace PracticeProblems
            
             foreach(int i in result)
             {
-                Console.Write(i + " ");
+                Console.Write(i + " , ");
+            }
+        }
+
+        public static void DeleteDoubleElements(double[]arr1, double delete1)
+        {
+
+            int k = 0;
+            double[] result = new double[arr1.Length - 1];
+
+            Console.WriteLine("The remaining elements are : ");
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                if (arr1[i] != delete1)
+                {
+                    result[k] = arr1[i];
+                    k++;
+                }
+            }
+
+            foreach (var i in result)
+            {
+                Console.Write(i + " , ");
+            }
+        }
+
+        public static void DeleteCharElements(char[] arr2, char delete1)
+        {
+
+            int k = 0;
+            char[] result = new char[arr2.Length - 1];
+
+            Console.WriteLine("The remaining elements are : ");
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                if (arr2[i] != delete1)
+                {
+                    result[k] = arr2[i];
+                    k++;
+                }
+            }
+
+            foreach (var i in result)
+            {
+                Console.Write(i + " , ");
             }
         }
     }
--- PracticeProblems/Program.cs
+++ PracticeProblems/Program.cs
@@ -14,13 +14,15 @@ namespace PracticeProblems
             {
                 
                
-                Console.WriteLine("\n \nChoose a number from following \n 1) To print remaining int elements after deleteing 1 element \n");
+                Console.WriteLine("\n \nChoose a number from following \n 1)  To Delete and print remaining array elements of int " +
+                    " \n 2)  To Delete and print remaining array elements of double  " +
+                    "\n 3)  To Delete and print remaining array elements of char \n");
 
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
                     case 1:
-                        Console.WriteLine("How many elements to add into array");
+                        Console.WriteLine("How many elements you want to add into array");
                         int size = Convert.ToInt32(Console.ReadLine());
                         int[] arr = new int[size];
 
@@ -30,9 +32,43 @@ namespace PracticeProblems
                             arr[i] = Convert.ToInt32(Console.ReadLine());
                         }
 
-                        Console.WriteLine("\n please pickup any one element from this array");
+                        Console.WriteLine("\n please pickup any one element that you want to delete");
                         int delete = Convert.ToInt32(Console.ReadLine());
                         DeleteArrayElements.DeleteIntElements(arr,delete);
+                        break;
+
+                    case 2:
+           
+                        Console.WriteLine("How many elements you want to add into array");
+                        int size1 = Convert.ToInt32(Console.ReadLine());
+                        double[] arr1 = new double[size1];
+
+                        Console.WriteLine("Enter the elements 1 by 1 :");
+                        for (int i = 0; i < size1; i++)
+                        {
+                            arr1[i] = Convert.ToDouble(Console.ReadLine());
+                        }
+
+                        Console.WriteLine("\n please pickup any one element that you want to delete");
+                        double delete1 = Convert.ToDouble(Console.ReadLine());
+                        DeleteArrayElements.DeleteDoubleElements(arr1, delete1);
+                        break;
+
+                    case 3:
+
+                        Console.WriteLine("How many elements you want to add into array");
+                        int size2 = Convert.ToInt32(Console.ReadLine());
+                        char[] arr2 = new char[size2];
+
+                        Console.WriteLine("Enter the letters 1 by 1 :");
+                        for (int i = 0; i < size2; i++)
+                        {
+                            arr2[i] = Convert.ToChar(Console.ReadLine());
+                        }
+
+                        Console.WriteLine("\n please pickup any one letter that you want to delete");
+                        char delete2 = Convert.ToChar(Console.ReadLine());
+                        DeleteArrayElements.DeleteCharElements(arr2, delete2);
                         break;
                 }
             }
